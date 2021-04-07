c350dc1a042fb4b0ed943b220327d1878074591f
Curtis Laurence Chadwell <>
2021-04-07 03:42:15 +0000

    Explained primeNums code

--- script.js
+++ script.js
@@ -2,9 +2,9 @@
 
 
 const celToFah = (temp) =>{
-  let faherheit = (temp * (9/5)) + 32
+  let fahernheit = (temp * (9/5)) + 32
 
-  return `${temp}°C converts to  ${faherheit}°F`
+  return `${temp}°C converts to  ${fahernheit}°F`
 
 }
 
@@ -28,24 +28,29 @@ console.log(sumArray(array))
 
 
 const primeNums = (num) =>{
+  //created an empty array to store prime values
   let primes = []
-  
+  //Off the bat...if num equals 1 should return false as prime numbers have to be greater than 1
   if (num === 1) return false
-  
+  //assuming num is greater than 1
   else if (num > 1){
+  //loop through numbers starting a second iteration based on rule above
   for (let i = 2; i < num; i++){
+  //will set isPrime to true by default
   let isPrime = true
+    //checking if the iteration its on is a prime number
     for (let j = 2; j < i; j++){
-   
+    //it that iteration is even....its automatically flagged as not prime
     if (i % j === 0) {
       isPrime = false
     } 
   }
-
+  //the ending number (num) is greater than 1 and the iteration (i) is prime...the current iteration (i) will get pushed into array
   if (num > 1 && isPrime === true) {
         primes.push(i)
     }
   }
+    //returns list of primes up to specified ending number (num)  
     return primes
   }
   
